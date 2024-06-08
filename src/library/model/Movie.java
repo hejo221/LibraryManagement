f55bfe33903f5246bec2fce87be7ad259e97b748
Henrik John <73546730+hejo221@users.noreply.github.com>
2024-06-08 22:47:22 +0200

    Update Movie.java

--- src/library/model/Movie.java
+++ src/library/model/Movie.java
@@ -10,6 +10,12 @@ public class Movie extends Media {
         this.lengthInMinutes = lengthInMinutes;
     }
 
+    public Movie(int mediaID, String title, int releaseYear, String director, int lengthInMinutes) {
+        super(mediaID, title, releaseYear);
+        this.director = director;
+        this.lengthInMinutes = lengthInMinutes;
+    }
+
     public String getDirector() {
         return director;
     }
@@ -24,6 +30,30 @@ public class Movie extends Media {
 
     public void setLengthInMinutes(int lengthInMinutes) {
         this.lengthInMinutes = lengthInMinutes;
+    }
+
+    @Override
+    public String getMediaType() {
+        return "Movie";
+    }
+
+    public static Movie readFromFile(String mediaString) {
+        String[] parts = mediaString.split(",");
+        if (parts.length != 6) {
+            throw new IllegalArgumentException("Invalid data format.");
+        }
+
+        int mediaID = Integer.parseInt(parts[1]);
+        String title = parts[2];
+        int releaseYear = Integer.parseInt(parts[3]);
+        String director = parts[4];
+        int lengthInMinutes = Integer.parseInt(parts[5]);
+
+        return new Movie(mediaID, title, releaseYear, director, lengthInMinutes);
+    }
+
+    public String writeToFile() {
+        return getMediaType() + "," + getMediaID() + "," + getTitle() + "," + getReleaseYear() + "," + getDirector() + "," + getLengthInMinutes();
     }
 
     @Override
