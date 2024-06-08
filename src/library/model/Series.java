37e459e9806468461f6f69bf6ca250d349038683
Henrik John <73546730+hejo221@users.noreply.github.com>
2024-06-08 22:47:47 +0200

    Update Series.java

--- src/library/model/Series.java
+++ src/library/model/Series.java
@@ -10,6 +10,12 @@ public class Series extends Media {
         this.numberOfEpisodes = numberOfEpisodes;
     }
 
+    public Series(int mediaID, String title, int releaseYear, int numberOfSeason, int numberOfEpisodes) {
+        super(mediaID, title, releaseYear);
+        this.numberOfSeason = numberOfSeason;
+        this.numberOfEpisodes = numberOfEpisodes;
+    }
+
     public int getNumberOfSeason() {
         return numberOfSeason;
     }
@@ -24,6 +30,30 @@ public class Series extends Media {
 
     public void setNumberOfEpisodes(int numberOfEpisodes) {
         this.numberOfEpisodes = numberOfEpisodes;
+    }
+
+    @Override
+    public String getMediaType() {
+        return "Series";
+    }
+
+    public static Series readFromFile(String mediaString) {
+        String[] parts = mediaString.split(",");
+        if (parts.length != 6) {
+            throw new IllegalArgumentException("Invalid data format.");
+        }
+
+        int mediaID = Integer.parseInt(parts[1]);
+        String title = parts[2];
+        int releaseYear = Integer.parseInt(parts[3]);
+        int numberOfSeason = Integer.parseInt(parts[4]);
+        int numberOfEpisodes = Integer.parseInt(parts[5]);
+
+        return new Series(mediaID, title, releaseYear, numberOfSeason, numberOfEpisodes);
+    }
+
+    public String writeToFile() {
+        return getMediaType() + "," + getMediaID() + "," + getTitle() + "," + getReleaseYear() + "," + getNumberOfSeason() + "," + getNumberOfEpisodes();
     }
 
     @Override
