bcb19b690ee18411937b7bdac17336e2fc5904de
Henrik John <73546730+hejo221@users.noreply.github.com>
2024-06-08 22:46:37 +0200

    Update Magazine.java

--- src/library/model/Magazine.java
+++ src/library/model/Magazine.java
@@ -8,12 +8,40 @@ public class Magazine extends Media {
         this.publisher = publisher;
     }
 
+    public Magazine(int mediaID, String title, int releaseYear, String publisher) {
+        super(mediaID, title, releaseYear);
+        this.publisher = publisher;
+    }
+
     public String getPublisher() {
         return publisher;
     }
 
     public void setPublisher(String publisher) {
         this.publisher = publisher;
+    }
+
+    @Override
+    public String getMediaType() {
+        return "Magazine";
+    }
+
+    public static Magazine readFromFile(String mediaString) {
+        String[] parts = mediaString.split(",");
+        if (parts.length != 5) {
+            throw new IllegalArgumentException("Invalid data format.");
+        }
+
+        int mediaID = Integer.parseInt(parts[1]);
+        String title = parts[2];
+        int releaseYear = Integer.parseInt(parts[3]);
+        String publisher = parts[4];
+
+        return new Magazine(mediaID, title, releaseYear, publisher);
+    }
+
+    public String writeToFile() {
+        return getMediaType() + "," + getMediaID() + "," + getTitle() + "," + getReleaseYear() + "," + getPublisher();
     }
 
     @Override
